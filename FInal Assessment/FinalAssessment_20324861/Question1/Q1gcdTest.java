68a8851a79c97d38b3de5d6aefd3463b73697a50
seananain <seann.anain@gmail.com>
2022-11-11 11:26:09 +0800

    q1 done

---
+++ FInal Assessment/FinalAssessment_20324861/Question1/Q1gcdTest.java
@@ -0,0 +1,20 @@
+public class Q1gcdTest 
+{
+    public static int GCD(int x, int y)
+    {
+        if(y==0)
+        {
+            return x;
+        }
+        System.out.println(x + " "+  y);
+        return GCD(y, x % y);
+    }
+
+    public static void main(String[] args)
+    {
+        int num1=2032, num2=4861;
+        System.out.println("Num 1: " + num1 + " Num 2: "+ num2);
+        System.out.println("GCD(num1, num2) = "+GCD(num1, num2));
+        System.out.println("GCD(num2, num1) = "+GCD(num2, num1));
+    }
+}
